2d1fab1b92758c516b116ffb13e3150321821473
namnhh <hoainam080695@gmail.com>
2017-05-18 13:38:35 +0700

    merge design pattern

---
+++ DesignPattern/Bai2/Chart1/data.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+	var text = [
+			'ĐÃ ĐẠT',   //text 1
+			'Chưa ĐẠT'  //text 2
+		];			
+	var textColor = "gray"; // color of text
+	var data = [
+			0.8,   // data of text 1
+			0.2    // data of text 2
+		];
+	var colors = [
+			"#03C", //top color of block text 1
+			"#0CF", //color of block text 1
+			"#F00", //top color of block text 2
+			"#FFB2B2", //color of block text 2
+			"#0CF", //color of line under text 1 
+			"#FFB2B2" //color of line under text 2
+		];
+	var font = "20px Arial"; // set font-style for text
+	var title = "BIỂU ĐỒ TỔNG QUÁT KHUNG NĂNG LỰC";
+	var titleColor = "blue";
+	
---
+++ DesignPattern/Bai2/Chart2/data.js
@@ -0,0 +1,14 @@
+﻿// JavaScript Document
+var itemData = [1.5, 3.5, 1.5, 3.5, 2.5, 3.5]; // data use to draw chart
+var itemValue = [ 0, 1, 2, 3, 4]; // data default in 0y. Show on Oy
+var colorLine = "#00AEEF"; // color of line behind column
+
+var colorItemValue = "black"; // set color for itemValue
+var fontItemValue = "20px Arial"; // set font  for itemValue
+
+var colorItemData = "black";//set color for itemName
+var fontItemData = "15px Arial"; // set font  for itemName
+
+var header = "RANK SCORE RANK"; // header
+var itemValueTitle = "HISTORY"; //title of itemName
+var itemDataTitle = "QUY LAM VIEC"; // title of itemValue
